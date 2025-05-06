33164a25c6ecf7ab2465b9ac321dcfa436175824
raffithoriq <thoriqibb@gmail.com>
2025-05-06 20:58:42 +0800

    first commit

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,27 +1,35 @@
 import React from "react";
-import GitHubIcon from '@mui/icons-material/GitHub';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import '../assets/styles/Main.scss';
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import "../assets/styles/Main.scss";
+import image from "../assets/images/1.jpeg";
 
 function Main() {
-
   return (
     <div className="container">
       <div className="about-section">
         <div className="image-wrapper">
-          <img src="https://my-aws-assets.s3.us-west-2.amazonaws.com/portfolio-img/avatar_circle.jpeg" alt="Avatar" />
+          <img src={image} alt="thoriq" />
         </div>
         <div className="content">
           <div className="social_icons">
-            <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-            <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            <a href="https://github.com/raffithoriq" target="_blank" rel="noreferrer">
+              <GitHubIcon />
+            </a>
+            <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer">
+              <LinkedInIcon />
+            </a>
           </div>
-          <h1>Yuji Sato</h1>
+          <h1>Achmad Roffi Thoriq</h1>
           <p>Full Stack Engineer</p>
 
           <div className="mobile_social_icons">
-            <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-            <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            <a href="https://github.com/raffithoriq" target="_blank" rel="noreferrer">
+              <GitHubIcon />
+            </a>
+            <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer">
+              <LinkedInIcon />
+            </a>
           </div>
         </div>
       </div>
